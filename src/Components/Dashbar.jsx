2321badc52b50fb0ca12f0eaec00d5e2a6c40ecd
Dhruv-413 <153450110+Dhruv-413@users.noreply.github.com>
2024-07-23 19:09:14 +0530

    Delete src directory

--- src/Components/Dashbar.jsx
+++
@@ -1,9 +0,0 @@
-export const DashBar = () => {
-    return (
-      <div className="text-black w-full">
-          <div className="text-3xl font-bold">
-              Welcome To Ecohive
-          </div>
-      </div>
-    );
-};
